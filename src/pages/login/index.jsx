23beaa808412d29cef2e495e64c830b2c1b25f06
Vishnu V <vishnu.veekancheeri@bizcloudexperts.com>
2023-12-14 17:28:19 +0530

    aplify-integration

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,45 +2,68 @@ import { Header } from "../../components";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./login.css";
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import auth from '../amplifyconfiguration.json'
+
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure({
+  Auth: {"region": "us-east-1",
+  "userPoolId": "us-east-1_nH3rxLIdf",
+  "userPoolWebClientId": "4baj7sgouh0r13pa0ohitvv2c",}
+})
+
+// function Login() {
+//   return (
+//     <div className="login-component">
+//       <div>
+//         <Header />
+//       </div>
+//       <div className="login-component-middle">
+//         <div className="login-component-middle-inner">
+//           <Form>
+//             <div className="login-component-form-header">
+//               <h3>Welcome back!</h3>
+//             </div>
+
+//             <Form.Group className="mb-3" controlId="formBasicEmail">
+//               <Form.Label>Email</Form.Label>
+//               <Form.Control
+//                 type="email"
+//                 placeholder="Enter your email address"
+//               />
+//             </Form.Group>
+
+//             <Form.Group className="mb-3" controlId="formBasicPassword">
+//               <Form.Label>Password</Form.Label>
+//               <Form.Control type="password" placeholder="Password" />
+//             </Form.Group>
+//             <div className="login-component-form-button">
+//               <Button
+//                 variant="primary"
+//                 type="submit"
+//                 id="login-component-form-button"
+//               >
+//                 Login
+//               </Button>
+//             </div>
+//           </Form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 function Login() {
   return (
-    <div className="login-component">
-      <div>
-        <Header />
-      </div>
-      <div className="login-component-middle">
-        <div className="login-component-middle-inner">
-          <Form>
-            <div className="login-component-form-header">
-              <h3>Welcome back!</h3>
-            </div>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter your email address"
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <div className="login-component-form-button">
-              <Button
-                variant="primary"
-                type="submit"
-                id="login-component-form-button"
-              >
-                Login
-              </Button>
-            </div>
-          </Form>
-        </div>
-      </div>
-    </div>
+    <Authenticator loginMechanisms={['username','email']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
 
