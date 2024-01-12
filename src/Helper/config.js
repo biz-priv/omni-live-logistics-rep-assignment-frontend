30212a1049946d014152246bc175a7ee9b4e1166
jahir-biz <jahir.uddin@bizcloudexperts.com>
2024-01-12 21:06:25 +0530

    auth added

--- src/Helper/config.js
+++ src/Helper/config.js
@@ -1,14 +1,28 @@
 import axios from "axios"
+import { fetchAuthSession } from "@aws-amplify/auth";
 
-const fetchClient = () => {
+async function getUser() {
+  const {
+      tokens: { idToken = "" },
+  } = await fetchAuthSession();
+  return idToken.toString()
+}
+
+const fetchClient = async() => {
   const defaultOptions = {
     baseURL: process.env.REACT_APP_BASE_URL,
+    headers:{
+      Authorization: await getUser(),
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      "Access-Control-Allow-Headers": "Content-Type", 
+    }
   };
   let instance = axios.create(defaultOptions);
   return instance;
 };
 
 export const apiConfig = {
-  get: async (path) => await fetchClient().get(path),
-  post: async (path,body) => await fetchClient().post(path, body)
+  get: async (path) => await (await fetchClient()).get(path),
+  post: async (path,body) => await (await fetchClient()).post(path, body)
 }
